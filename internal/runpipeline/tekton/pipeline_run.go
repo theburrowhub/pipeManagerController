434a27a81a9d12f2a0f824926b3232dd8057cae0
Sergio Tejón <sergio.tejon@gmail.com>
2024-11-30 00:23:07 +0100

    chore(internal/runpipeline/tekton/pipeline_run.go): Update package imports and remove unused import
    - Remove "gopkg.in/yaml.v3" import
    - Add "encoding/json", "fmt", and v1 from "k8s.io/api/core/v1" and "metav1" from "k8s.io/apimachinery/pkg/apis/meta/v1"
    - Update metav1 to use k8s.io instead of gopkg.in for imports
    - Minor refactoring of import section for better organization and consistency

--- internal/runpipeline/tekton/pipeline_run.go
+++ internal/runpipeline/tekton/pipeline_run.go
@@ -1,17 +1,23 @@
 package tekton
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-logr/logr"
-	"gopkg.in/yaml.v3"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/selection"
+	"os"
 
 	"github.com/sergiotejon/pipeManagerController/api/v1alpha1"
 
 	tektonpipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 )
 
-const workspaceName = "workspace"
+const (
+	workspaceName = "workspace"
+	workingDir    = "/workspaceDir"
+)
 
 var (
 	logger   *logr.Logger
@@ -23,28 +29,6 @@ func Deploy(l *logr.Logger, p *v1alpha1.PipelineSpec) error {
 
 	logger = l
 	pipeline = p
-
-	//// Aquí puedes iterar sobre las tareas y gestionar cada una
-	//for taskName, task := range pipeline.Tasks {
-	//	logger.Info("Processing Task", "TaskName", taskName, "Description", task.Description)
-	//	for _, step := range task.Steps {
-	//		logger.Info("  Step", "Name", step.Name, "Image", step.Image)
-	//		// Implementa la lógica para manejar cada step
-	//		// Por ejemplo, crear Pods o Jobs según los steps
-	//	}
-	//}
-	//
-
-	// DEBUG
-	fmt.Println("-------> Pipeline:")
-	// Marshal the normalizedPipelineSpec to YAML
-	yamlData, err := yaml.Marshal(pipeline)
-	if err != nil {
-		logger.Error(err, "Error marshaling normalizedPipelineSpec to YAML")
-	} else {
-		fmt.Println(string(yamlData))
-	}
-	// DEBUG
 
 	tektonPipelineRun := tektonpipelinev1.PipelineRun{
 		TypeMeta: metav1.TypeMeta{
@@ -59,17 +43,6 @@ func Deploy(l *logr.Logger, p *v1alpha1.PipelineSpec) error {
 		Status: tektonpipelinev1.PipelineRunStatus{},
 	}
 
-	// DEBUG
-	fmt.Println("-------> PipelineRun:")
-	// Marshal the normalizedPipelineSpec to YAML
-	yamlData, err = yaml.Marshal(tektonPipelineRun)
-	if err != nil {
-		logger.Error(err, "Error marshaling normalizedPipelineSpec to YAML")
-	} else {
-		fmt.Println(string(yamlData))
-	}
-	// DEBUG
-
 	err = runDeploy(&tektonPipelineRun)
 	if err != nil {
 		logger.Error(err, "Error deploying PipelineRun")
@@ -81,14 +54,30 @@ func Deploy(l *logr.Logger, p *v1alpha1.PipelineSpec) error {
 
 func runDeploy(pipelineRun *tektonpipelinev1.PipelineRun) error {
 	// TODO: Implement the logic to deploy the PipelineRun to the Kubernetes cluster
+	// DEBUG
+	fmt.Println("-------> PipelineRun:")
+	// Marshal the normalizedPipelineSpec to YAML
+	jsonData, err := json.MarshalIndent(pipelineRun, "", " ")
+	if err != nil {
+		logger.Error(err, "Error marshaling normalizedPipelineSpec to JSON")
+	} else {
+		err = os.WriteFile("/tmp/file.json", jsonData, 0666)
+		if err != nil {
+			logger.Error(err, "No funciona esto")
+		}
+	}
+	// DEBUG
+
 	return nil
 }
 
 func buildPipelineRunSpec() tektonpipelinev1.PipelineRunSpec {
 	spec := tektonpipelinev1.PipelineRunSpec{
 		Params:     buildParams(),
-		Workspaces: buildWorkspaces(),
-		// TODO: implement the rest...
+		Workspaces: buildWorkspacesBinding(),
+		// TODO: Implement timeouts by global config and pipeline configuration as optional
+		//Timeouts: ...
+		PipelineSpec: buildPipelineSpec(),
 	}
 
 	return spec
@@ -110,7 +99,7 @@ func buildParams() []tektonpipelinev1.Param {
 	return params
 }
 
-func buildWorkspaces() []tektonpipelinev1.WorkspaceBinding {
+func buildWorkspacesBinding() []tektonpipelinev1.WorkspaceBinding {
 	var workspaces []tektonpipelinev1.WorkspaceBinding
 
 	// TODO: test this code with a pipeline that has a workspace defined in the configuration section
@@ -128,4 +117,183 @@ func buildWorkspaces() []tektonpipelinev1.WorkspaceBinding {
 	})
 
 	return workspaces
+}
+
+func buildPipelineSpec() *tektonpipelinev1.PipelineSpec {
+	pipelineSpec := &tektonpipelinev1.PipelineSpec{
+		Description: pipeline.Description,
+		DisplayName: pipeline.Name,
+		Tasks:       buildTasks(),
+		Finally:     buildFinallyTasks(),
+	}
+
+	return pipelineSpec
+}
+
+func buildTasks() []tektonpipelinev1.PipelineTask {
+	var tasks []tektonpipelinev1.PipelineTask
+
+	for name, taskData := range pipeline.Tasks {
+		tasks = append(tasks, tektonpipelinev1.PipelineTask{
+			Name:        name,
+			DisplayName: name,
+			Description: taskData.Description,
+			RunAfter:    taskData.RunAfter,
+			TaskSpec:    buildTaskSpec(name, taskData),
+			// TODO:
+			// Retries:
+		})
+
+	}
+	return tasks
+}
+
+func buildFinallyTasks() []tektonpipelinev1.PipelineTask {
+	var tasks []tektonpipelinev1.PipelineTask
+
+	for name, taskData := range pipeline.FinishTasks.Success {
+		tasks = append(tasks, tektonpipelinev1.PipelineTask{
+			Name:        name,
+			DisplayName: name,
+			Description: taskData.Description,
+			RunAfter:    taskData.RunAfter,
+			TaskSpec:    buildTaskSpec(name, taskData),
+			When:        buildWhenSuccessExpresion(),
+			// TODO:
+			// Retries:
+		})
+	}
+
+	for name, taskData := range pipeline.FinishTasks.Fail {
+		tasks = append(tasks, tektonpipelinev1.PipelineTask{
+			Name:        name,
+			DisplayName: name,
+			Description: taskData.Description,
+			RunAfter:    taskData.RunAfter,
+			TaskSpec:    buildTaskSpec(name, taskData),
+			When:        buildWhenFailExpresion(),
+			// TODO:
+			// Retries:
+		})
+	}
+
+	return tasks
+}
+
+func buildTaskSpec(name string, data v1alpha1.Task) *tektonpipelinev1.EmbeddedTask {
+	return &tektonpipelinev1.EmbeddedTask{
+		TaskSpec: tektonpipelinev1.TaskSpec{
+			DisplayName: name,
+			Description: data.Description,
+			Params:      buildTaskParams(data),
+			Steps:       buildTaskSteps(data),
+			Volumes:     buildTaskVolumes(data),
+			Sidecars:    buildTaskSidecars(data),
+		},
+	}
+}
+
+func buildTaskParams(data v1alpha1.Task) tektonpipelinev1.ParamSpecs {
+	var paramSpecs tektonpipelinev1.ParamSpecs
+
+	for paramName, _ := range data.Params {
+		paramSpecs = append(paramSpecs, tektonpipelinev1.ParamSpec{
+			Name: paramName,
+			Type: tektonpipelinev1.ParamTypeString,
+		})
+	}
+
+	return paramSpecs
+}
+
+func buildTaskSteps(data v1alpha1.Task) []tektonpipelinev1.Step {
+	var steps []tektonpipelinev1.Step
+
+	for _, stepData := range data.Steps {
+		steps = append(steps, tektonpipelinev1.Step{
+			Name:         stepData.Name,
+			Image:        stepData.Image,
+			Env:          stepData.Env,
+			Command:      stepData.Command,
+			Args:         stepData.Args,
+			Script:       stepData.Script,
+			VolumeMounts: stepData.VolumeMounts,
+			WorkingDir:   workingDir,
+
+			// TODO:
+			//ComputeResources:
+			//Timeout: (task timeout)
+			//When:
+		})
+	}
+
+	return steps
+}
+
+func buildTaskVolumes(data v1alpha1.Task) []v1.Volume {
+	var volumes []v1.Volume
+
+	volumes = append(volumes, pipeline.Workspace)
+	for _, volume := range data.Volumes {
+		volumes = append(volumes, volume)
+	}
+
+	return volumes
+}
+
+func buildTaskSidecars(data v1alpha1.Task) []tektonpipelinev1.Sidecar {
+	var sidecars []tektonpipelinev1.Sidecar
+
+	for _, sidecar := range data.Sidecars {
+		sidecars = append(sidecars, tektonpipelinev1.Sidecar{
+			Name:                     sidecar.Name,
+			Image:                    sidecar.Image,
+			Command:                  sidecar.Command,
+			Args:                     sidecar.Args,
+			WorkingDir:               sidecar.WorkingDir,
+			Ports:                    sidecar.Ports,
+			EnvFrom:                  sidecar.EnvFrom,
+			Env:                      sidecar.Env,
+			VolumeMounts:             sidecar.VolumeMounts,
+			VolumeDevices:            sidecar.VolumeDevices,
+			LivenessProbe:            sidecar.LivenessProbe,
+			ReadinessProbe:           sidecar.ReadinessProbe,
+			StartupProbe:             sidecar.StartupProbe,
+			Lifecycle:                sidecar.Lifecycle,
+			TerminationMessagePath:   sidecar.TerminationMessagePath,
+			TerminationMessagePolicy: sidecar.TerminationMessagePolicy,
+			ImagePullPolicy:          sidecar.ImagePullPolicy,
+			SecurityContext:          sidecar.SecurityContext,
+			Stdin:                    sidecar.Stdin,
+			StdinOnce:                sidecar.StdinOnce,
+			TTY:                      sidecar.TTY,
+			RestartPolicy:            sidecar.RestartPolicy,
+		})
+	}
+
+	return sidecars
+}
+
+func buildWhenSuccessExpresion() tektonpipelinev1.WhenExpressions {
+	var exp tektonpipelinev1.WhenExpressions
+
+	exp = append(exp, tektonpipelinev1.WhenExpression{
+		Input:    "$(tasks.status)",
+		Operator: selection.NotIn,
+		Values:   []string{"Failed"},
+	})
+
+	return exp
+}
+
+func buildWhenFailExpresion() tektonpipelinev1.WhenExpressions {
+	var exp tektonpipelinev1.WhenExpressions
+
+	exp = append(exp, tektonpipelinev1.WhenExpression{
+		Input:    "$(tasks.status)",
+		Operator: selection.In,
+		Values:   []string{"Failed"},
+	})
+
+	return exp
 }
