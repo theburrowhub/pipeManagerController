70b66750e6f0092d3dfc10b2b9eeda755416a0b0
Sergio Tejón <sergio.tejon@gmail.com>
2024-12-09 14:22:00 +0100

    refactor(internal/normalize/functions.go): Import K8s API and organize code for better readability
    - Add github.com/sergiotejon/pipeManagerController/api/v1alpha1 import
    - Move k8sObjectName function to a more appropriate location
    - Introduce getTaskNames function to extract task names from a map of tasks

--- internal/normalize/functions.go
+++ internal/normalize/functions.go
@@ -1,5 +1,7 @@
 package normalize
 
+import "github.com/sergiotejon/pipeManagerController/api/v1alpha1"
+
 // k8sObjectName returns a valid Kubernetes object name by combining two strings
 func k8sObjectName(str1, str2 string) string {
 	combined := str1 + "-" + str2
@@ -7,4 +9,13 @@ func k8sObjectName(str1, str2 string) string {
 		return combined[:60]
 	}
 	return combined
+}
+
+// getTaskNames returns a slice of task names from a map of tasks
+func getTaskNames(tasks map[string]v1alpha1.Task) []string {
+	var taskNames []string
+	for name := range tasks {
+		taskNames = append(taskNames, name)
+	}
+	return taskNames
 }
