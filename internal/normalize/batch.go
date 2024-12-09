048b243a5cb2b37ec38b75cf5b2da5226b314fb6
Sergio Tejón <sergio.tejon@gmail.com>
2024-12-09 14:17:55 +0100

    fix(internal/normalize/batch.go): Implement runAfter reference adjustments for batch tasks
    - Modify pipeline task RunAfter references if they point to the specified batch task
    - Adjust FinishTasks.Fail and FinishTasks.Success RunAfter references similarly

--- internal/normalize/batch.go
+++ internal/normalize/batch.go
@@ -30,3 +30,42 @@ func processBatchTask(taskName string, taskData v1alpha1.Task) map[string]v1alph
 
 	return tasks
 }
+
+// fixRunAfterReferencesForBatchTasks fixes the runAfter references in the pipeline
+// TODO: Mix FinishTasks and Tasks in the same type of data
+// TODO: Refactor this function to avoid code duplication
+func fixRunAfterReferencesForBatchTasks(pipeline *v1alpha1.PipelineSpec, batchTaskName string, newTaskNames []string) {
+	for t := range pipeline.Tasks {
+		for i := range pipeline.Tasks[t].RunAfter {
+			if pipeline.Tasks[t].RunAfter[i] == batchTaskName {
+				newRunAfter := append(pipeline.Tasks[t].RunAfter[:i], append(newTaskNames, pipeline.Tasks[t].RunAfter[i+1:]...)...)
+				task := pipeline.Tasks[t]
+				task.RunAfter = newRunAfter
+				pipeline.Tasks[t] = task
+				break
+			}
+		}
+	}
+	for t := range pipeline.FinishTasks.Fail {
+		for i := range pipeline.FinishTasks.Fail[t].RunAfter {
+			if pipeline.FinishTasks.Fail[t].RunAfter[i] == batchTaskName {
+				newRunAfter := append(pipeline.FinishTasks.Fail[t].RunAfter[:i], append(newTaskNames, pipeline.FinishTasks.Fail[t].RunAfter[i+1:]...)...)
+				task := pipeline.FinishTasks.Fail[t]
+				task.RunAfter = newRunAfter
+				pipeline.FinishTasks.Fail[t] = task
+				break
+			}
+		}
+	}
+	for t := range pipeline.FinishTasks.Success {
+		for i := range pipeline.FinishTasks.Success[t].RunAfter {
+			if pipeline.FinishTasks.Success[t].RunAfter[i] == batchTaskName {
+				newRunAfter := append(pipeline.FinishTasks.Success[t].RunAfter[:i], append(newTaskNames, pipeline.FinishTasks.Success[t].RunAfter[i+1:]...)...)
+				task := pipeline.FinishTasks.Success[t]
+				task.RunAfter = newRunAfter
+				pipeline.FinishTasks.Success[t] = task
+				break
+			}
+		}
+	}
+}
